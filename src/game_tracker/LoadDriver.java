08b0c0ea9a489a29d45f8a5c4a0e81873bc4d283
Zachary Behnke <ztbehnke@gmail.com>
2016-06-19 11:46:52 -0500

    Revert "Add"
    This reverts commit cf10b1864249d3d6ce197945d19cf774be978e5c.

--- src/game_tracker/LoadDriver.java
+++ src/game_tracker/LoadDriver.java
@@ -112,7 +112,6 @@ public class LoadDriver extends JPanel implements DocumentListener
 			
 			selectQuery();
 	}
-	
 	@SuppressWarnings("static-access")
 	public void selectQuery() throws SQLException
 	{
